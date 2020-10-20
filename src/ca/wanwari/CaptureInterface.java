37490c2ac27e2d7966993787288cb1c01f903e9b
wiesa56 <contact@wanwari.ca>
2020-10-19 23:17:32 -0400

    refactor: small refactor of CaptureInterface.java variables

--- src/ca/wanwari/CaptureInterface.java
+++ src/ca/wanwari/CaptureInterface.java
@@ -8,15 +8,15 @@ import java.awt.event.MouseEvent;
 public class CaptureInterface extends MouseAdapter {
 
     JFrame displayWindow;
-    DrawPanel drawPanel = new DrawPanel();
-    final int width = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-    final int height = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+    DrawPanel drawPanel;
+    final int screen_width = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    final int screen_height = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
 
     CaptureInterface() {
         displayWindow = new JFrame();
         displayWindow.setUndecorated(true);
         displayWindow.setLocation(new Point(0, 0));
-        displayWindow.setSize(new Dimension(width, height));
+        displayWindow.setSize(new Dimension(screen_width, screen_height));
         displayWindow.setOpacity(0.3f);
         displayWindow.addMouseListener(this);
         displayWindow.addMouseMotionListener(this);
@@ -45,6 +45,7 @@ public class CaptureInterface extends MouseAdapter {
     @Override
     public void mouseDragged(MouseEvent mouseEvent) {
         super.mouseDragged(mouseEvent);
+        System.out.println("Dragged to: " + mouseEvent.getPoint());
         drawPanel.setWidth(mouseEvent.getX());
         drawPanel.setHeight(mouseEvent.getY());
 
