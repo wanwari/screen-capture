12096af7998d0dd674e0bf5f0e5782c57a69fcd9
wiesa56 <contact@wanwari.ca>
2020-10-19 21:24:23 -0400

    feat: check if system supports translucency

--- src/ca/wanwari/Main.java
+++ src/ca/wanwari/Main.java
@@ -1,8 +1,28 @@
 package ca.wanwari;
 
+import java.awt.*;
+import static java.awt.GraphicsDevice.WindowTranslucency.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        GraphicsEnvironment ge =
+                GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice gd = ge.getDefaultScreenDevice();
+
+        boolean isUniformTranslucencySupported =
+                gd.isWindowTranslucencySupported(TRANSLUCENT);
+        boolean isPerPixelTranslucencySupported =
+                gd.isWindowTranslucencySupported(PERPIXEL_TRANSLUCENT);
+        boolean isShapedWindowSupported =
+                gd.isWindowTranslucencySupported(PERPIXEL_TRANSPARENT);
+
+        System.out.println("TRANSLUCENT " + isUniformTranslucencySupported);
+        System.out.println("PERPIXEL_TRANSLUCENT " + isPerPixelTranslucencySupported);
+        System.out.println("PERPIXEL_TRANSPARENT " + isShapedWindowSupported);
+
+        if (!isUniformTranslucencySupported)
+            System.exit(-1);
     }
 }
