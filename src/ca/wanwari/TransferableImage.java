5cf6d9fbd9c001e938ac3e73c34cc019f6d69552
wiesa56 <contact@wanwari.ca>
2020-10-26 17:06:12 -0400

    create(TransferableImage): implement Transferable so a bufferedimage can be used in a transfer operation

---
+++ src/ca/wanwari/TransferableImage.java
@@ -0,0 +1,65 @@
+/*
+    TransferableImage.java
+    implements the interface for Transferable so the data (BufferedImage) can be used in a transfer operation
+    refer to the Transferable and DataFlavor APIs
+    https://docs.oracle.com/en/java/javase/15/docs/api/java.datatransfer/java/awt/datatransfer/Transferable.html
+    https://docs.oracle.com/en/java/javase/15/docs/api/java.datatransfer/java/awt/datatransfer/DataFlavor.html
+ */
+
+package ca.wanwari;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+class TransferableImage implements Transferable {
+
+    BufferedImage imageToTransfer;
+
+    /*
+        TransferableImage(BufferedImage imageToTransfer)
+        sets the BufferedImage within the object to the BufferedImage received
+     */
+    TransferableImage(BufferedImage imageToTransfer) {
+        this.imageToTransfer = imageToTransfer;
+    }
+
+    /*
+        public DataFlavor[] getTransferDataFlavors()
+        'Returns an array of DataFlavor objects indicating the flavors the data can be provided in'
+        object contains imageFlavor as the chosen DataFlavor
+        DataFlavor.imageFlavor represents a Java Image class
+     */
+    @Override
+    public DataFlavor[] getTransferDataFlavors() {
+        return new DataFlavor[] { DataFlavor.imageFlavor };
+    }
+
+    /*
+        public boolean isDataFlavorSupported(DataFlavor flavor)
+        'Returns whether or not the specified data flavor is supported for this object'
+        returns whether or not the DataFlavor passed is equal to an imageFlavor
+     */
+    @Override
+    public boolean isDataFlavorSupported(DataFlavor flavor) {
+        return flavor == DataFlavor.imageFlavor;
+    }
+
+    /*
+        public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException
+        'Returns an object which represents the data to be transferred. The class of the object returned is defined by
+         the representation class of the flavor'
+         if isDataFlavorSupported returns true return the BufferedImage
+         else throw an UnsupportedFlavorException
+     */
+    @Override
+    public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+        if (isDataFlavorSupported(flavor)) {
+            return imageToTransfer;
+        } else {
+            throw new UnsupportedFlavorException(flavor);
+        }
+    }
+}
